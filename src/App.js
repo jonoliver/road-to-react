5b6d18b57ca7708648abbc2c01885f2175d28fb8
Jon Oliver <imjonoliver@gmail.com>
2017-04-29 12:45:38 -0400

    Formatting

--- src/App.js
+++ src/App.js
@@ -36,27 +36,27 @@ const isSearched = (searchTerm)  => (item) =>
     }
   }
 
-  class Table extends Component {
-    render(){
-      const { list, pattern, onDismiss } = this.props;
-      return(
-        <div>
-          { list.filter(isSearched(pattern)).map(item =>
-            <div id='list' key={ item.objectID }>
-              <a href="{ item.url }">{ item.title }</a>
-              <span>{ item.author }</span>
-              <span>{ item.num_comments }</span>
-              <span>{ item.points }</span>
-              <span>
-                <button onClick={ () => onDismiss(item.objectID) }
-                  type='button'>Dismiss</button>
-              </span>
-            </div>
-          )}
-        </div>
-      )
-    }
+class Table extends Component {
+  render(){
+    const { list, pattern, onDismiss } = this.props;
+    return(
+      <div>
+        { list.filter(isSearched(pattern)).map(item =>
+          <div id='list' key={ item.objectID }>
+            <a href="{ item.url }">{ item.title }</a>
+            <span>{ item.author }</span>
+            <span>{ item.num_comments }</span>
+            <span>{ item.points }</span>
+            <span>
+              <button onClick={ () => onDismiss(item.objectID) }
+                type='button'>Dismiss</button>
+            </span>
+          </div>
+        )}
+      </div>
+    )
   }
+}
 
 class App extends Component {
   constructor(props) {
