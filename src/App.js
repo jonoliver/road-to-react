7837e730c7eda5b09c9fc8bb02e3fbfb40118fa7
Jon Oliver <imjonoliver@gmail.com>
2017-04-28 23:17:39 -0400

    Add list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'Github',
+    url: 'https://github.com/jonoliver',
+    author: 'Jon Oliver',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Blog',
+    url: 'http://jonoliver.codes',
+    author: 'Jon Oliver',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+  render = () =>
+    <div className="App">
+      { list.map(item =>
+        <div id='list' key={ item.objectID }>
+          <a href="{ item.url }">{ item.title }</a>
+          <span>{ item.author }</span>
+          <span>{ item.num_comments }</span>
+          <span>{ item.points }</span>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+      )}
+    </div>
 }
 
 export default App;
